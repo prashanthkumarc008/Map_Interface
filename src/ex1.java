6b48f4bac1dc2f18c4d85f7780115becc86cd193
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-07-27 12:15:08 +0530

    updated

---
+++ src/ex1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ex1 {
+    public static void main(String[] args) {
+        HashMap<Integer,String> map = new HashMap<>(Map.of(101,"pacchu",
+                                                            102,"anada",
+                                                            103,"dinesha"));
+        System.out.println(map);
+    }
+}
