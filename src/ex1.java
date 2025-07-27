1a65e89bf7ab004a109d8ba6080ece783a699700
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-07-27 12:26:54 +0530

    updated

--- src/ex1.java
+++ src/ex1.java
@@ -3,12 +3,17 @@ import java.util.Map;
 
 public class ex1 {
     public static void main(String[] args) {
-        HashMap<Integer,String> map = new HashMap<>(Map.of(101,"pacchu",
-                                                            102,"anada",
-                                                            103,"dinesha"));
+        HashMap<Integer, String> map = new HashMap<>(Map.of(
+                101, "pacchu",
+                102, "anada",
+                103, "dinesha"
+        ));
         System.out.println(map);
-        System.out.println(map.get(101));
-        System.out.println(map.get(102));
-        System.out.println(map.get(103));
+
+        // For-each loop to iterate over map entries
+        for (Map.Entry<Integer, String> e : map.entrySet()) {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
     }
 }
