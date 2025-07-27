6dea1b2400ede5fbe6584c5f48314f5aed9414fa
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-07-27 12:28:05 +0530

    updated

--- src/ex1.java
+++ src/ex1.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class ex1 {
@@ -6,7 +7,9 @@ public class ex1 {
         HashMap<Integer, String> map = new HashMap<>(Map.of(
                 101, "pacchu",
                 102, "anada",
-                103, "dinesha"
+                103, "dinesha",
+                104, "carol",
+                105, "dave"
         ));
         System.out.println(map);
 
@@ -14,6 +17,14 @@ public class ex1 {
         for (Map.Entry<Integer, String> e : map.entrySet()) {
             System.out.println(e.getKey());
             System.out.println(e.getValue());
+        }
+
+        // Iterator-based loop with proper generics
+        Iterator<Map.Entry<Integer, String>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Integer, String> e1 = it.next();
+            System.out.println(e1.getKey());
+            System.out.println(e1.getValue());
         }
     }
 }
